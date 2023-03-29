4eadd71bebc8a05a0cc8b0980e2b5e35854342a1
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-03-29 14:24:44 +0700

    Add project 2

--- project_2/entity/book.go
+++ project_2/entity/book.go
@@ -4,7 +4,7 @@ import "time"
 
 type Book struct {
 	Id        uint      `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title" gorm:"not null;unique;type:varchar(200)"`
+	NameBook  string    `json:"name_book" gorm:"not null;unique;type:varchar(200)"`
 	Author    string    `json:"author" gorm:"not null;unique;type:varchar(200)"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
