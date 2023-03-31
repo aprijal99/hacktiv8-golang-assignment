89c527858e23ba7df5e86481e54b08d2beb7bb9d
Aprijal Ghiyas Setiawan <aprijalghiyas@gmail.com>
2023-03-31 10:00:48 +0700

    Add assignment 6

--- assignment_6/database/database.go
+++ assignment_6/database/database.go
@@ -3,13 +3,15 @@ package database
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 const (
 	host     = "localhost"
 	port     = 5432
 	user     = "postgres"
-	password = "postgres"
+	password = "subang12345"
 	dbname   = "book-service"
 )
 
